6bfe39a74e2d88b70a874f5ddd400a3d0c8acee0
Murillo Javier <jajomuri@espol.edu.ec>
2024-08-31 13:44:16 -0500

    Registration and login

--- src/main/java/com/globant/model/User.java
+++ src/main/java/com/globant/model/User.java
@@ -102,24 +102,12 @@ public class User {
         return email;
     }
 
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
     public String getPassword() {
         return password;
     }
 
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
     public int getId() {
         return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
     }
 
     public DigitalWallet getDigitalWallet() {
